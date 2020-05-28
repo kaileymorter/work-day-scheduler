e108e5e5e61dabcf6440943d55ab4581caa1b7e6
Spartacus <k_morter@outlook.com>
2020-05-28 11:37:54 -0600

    added timeblocks

--- Develop/script.js
+++ Develop/script.js
@@ -3,3 +3,10 @@ $('#currentDay').text(moment().format("dddd, MMMM DD, YYYY"));
 
 //time blocks
 
+//edit hour schedule
+$('.description').on('click', 'p', function() {
+    var text = $(this).text().trim();
+    var textInput = $('<textarea>').val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger('focus');
+  });
