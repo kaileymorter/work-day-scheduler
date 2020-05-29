f1f8bca435155fa72297704a739c423a3116da15
Spartacus <k_morter@outlook.com>
2020-05-29 12:53:20 -0600

    added color coding time function

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,56 @@
-//current day
-$('#currentDay').text(moment().format("dddd, MMMM DD, YYYY"));
+//variables
+var descriptionEl = $('.description').find('textarea');
 
-//time blocks
+//set current date
+$('#currentDay').text(moment().format("dddd, MMM Do YYYY"));
 
-//edit hour schedule
-$('.description').on('click', 'p', function() {
-    var text = $(this).text().trim();
-    var textInput = $('<textarea>').val(text);
-    $(this).replaceWith(textInput);
-    textInput.trigger('focus');
-  });
+//color-code time blocks
+var workHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
+auditSchedule();
+
+function auditSchedule(){
+  var currentTime = moment().format('H')
+  for (var i = 0; i < workHours.length; i++) {
+    if (parseInt(workHours[i]) > currentTime) {
+      $("#" + workHours[i]).addClass('future');
+    } else if (parseInt(workHours[i]) < currentTime) {
+      $("#" + workHours[i]).addClass('past');
+    }
+  }
+}
+
+//save work tasks to localStorage
+$('.saveBtn').on('click', function(){
+  var workHour = $('#work-hour').text().trim();
+  var workTask = $('textarea').val().trim();
+  localStorage.setItem(workHour, workTask);
+  console.log(workHour, JSON.stringy(workTask));
+})
+
+//task variable that holds the task items for each hour
+var tasks = JSON.parse(localStorage.getItem('work-schedule')) || [];
+
+//Add work task to hour
+$('.description').on('click', 'p', function(taskInput) {
+  var task = $(this).text().trim();
+  var taskInput = $('<textarea>').val(task);
+  $(this).replaceWith(taskInput);
+  taskInput.trigger('focus');
+});
+
+  // //save button was clicked
+  // $('.saveBtn').click(function(e){
+  //   //get description value
+  //   var text = $('.description').find('textarea').val();
+
+  //   schedule = text;
+
+  //   console.log(schedule);
+
+  //   saveSchedule();
+  // })
+
+  // //save schedule to localStorage
+  // var saveSchedule = function() {
+  //   localStorage.setItem("schedule", JSON.stringify(schedule));
+  // };
